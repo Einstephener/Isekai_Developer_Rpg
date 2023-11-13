e31debb3ab4ae387fd1913e03508d8b6663dd9be
Einstephener <kihyuk0201@gmail.com>
2023-11-13 11:22:57 +0900

    이세계개발자

---
+++ GPTRPG/ICharacter.cs
@@ -0,0 +1,14 @@
+﻿public interface ICharacter
+{
+    int Atk { get; }
+    int Def { get; }
+    int Gold { get; set; }
+    int Hp { get; set; }
+    List<Item> Inventory { get; }
+    string Job { get; }
+    int Level { get; }
+    string Name { get; }
+
+    void AddToInventory(Item item);
+    void EquipItem(int input);
+}
---
+++ GPTRPG/IItem.cs
@@ -0,0 +1,9 @@
+﻿public interface IItem
+{
+    bool IsEquipped { get; set; }
+    int ItemAtk { get; }
+    int ItemDef { get; }
+    string ItemDescription { get; set; }
+    int ItemGold { get; }
+    string ItemName { get; }
+}
